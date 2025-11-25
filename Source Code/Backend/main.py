cfbd372efb43b68dfcaaf53ed4b1b1e3f6bd6673
Zell-Adam23 <adamzell1223@gmail.com>
2025-11-25 16:40:50 -0600

    connect python to supabase

--- Source Code/Backend/main.py
+++ Source Code/Backend/main.py
@@ -1,5 +1,24 @@
 # main.py
 
 # this file will be responisble for launching/managing the database
+from supabase import create_client
+from dotenv import load_dotenv
+import os
 
-import psycopg2
+def main():
+    load_dotenv()
+
+
+    supabase_url = os.getenv("SUPABASE_URL")
+    supabase_service_key = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+
+    if not supabase_url or not supabase_service_key:
+        print("invalid keys")
+    
+    connection = create_client(supabase_url, supabase_service_key)
+
+    response = connection.table("recipes").select("*").execute()
+    print (response.data)
+
+if __name__ == "__main__":
+    main()
