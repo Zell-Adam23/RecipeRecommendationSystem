68dcac468021a679710dc8c137839aebc9836610
Zell-Adam23 <adamzell1223@gmail.com>
2025-12-15 00:14:50 -0600

    renamed source-code to Source_code

--- Testing/Test_client.py
+++ Testing/Test_client.py
@@ -1,18 +1,4 @@
-import sys
-import os
 import pytest
+from Source_Code.Backend.backend_connections import app
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../Source-Code/Backend"))
-
-from Backend_connections import app
-
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
-
-def test_get_all_recipes(client):
-    response = client.get("/api/recipes")
-    assert response.status_code == 200
-    data = response.get_json()
-    assert isinstance(data, list)
+#new tests here
